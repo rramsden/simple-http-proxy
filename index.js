0f04ccfe44f77ce3a8211b19d7e235da9f6a4c57
Richard Ramsden <richard@rramsden.ca>
2021-06-27 15:39:30 +0900

    First working version

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require('express')
 const app = express()
-const request = require('request');
+const basicAuth = require('express-basic-auth')
+const express = require('express')
+const request = require('request')
 
-const ENDPOINT = process.env.HOST
+const API_KEY = process.env.API_KEY
+if (API_KEY == null) {
+  console.log(`Please specify ${API_KEY}`)
+  process.exit(1)
+}
 
-app.get('*', function(req,res) {
-  var newurl = ENDPOINT + req.originalUrl
-  request(newurl).pipe(res);
-})
+// Setup Basic Auth
+let auth = { users: {} }
+auth.users[API_KEY] = ''
+app.use(basicAuth(auth))
 
-app.post('*', function(req,res) {
-  var newurl = ENDPOINT + req.originalUrl
-  request(newurl).pipe(res);
-})
+// Proxy Function
+function proxy(req, res) {
+  const endpoint = req.header('X-Proxy-Url')
+
+  if (endpoint) {
+    var newurl = endpoint + req.originalUrl
+    request(newurl).pipe(res);
+  } else {
+    res.status(500)
+    res.json({error: 'Please set X-Proxy-Url'})
+  }
+}
+
+app.get('*', function(req, res) { proxy(req, res) })
+app.post('*', function(req, res) { proxy(req, res) })
 
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => {
